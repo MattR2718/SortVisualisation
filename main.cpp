de92072c7c47cc0fc110b04b7a2ead524598e67c
Matthew Ranson <matthew.ranson2718@gmail.com>
2022-04-12 14:53:31 +0100

    init

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <thread>
+#include <X11/Xlib.h>
+
+int main()
+{
+    // Create the main window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    // Start the game loop
+    while (window.isOpen())
+    {
+        // Process events
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // Close window: exit
+            if (event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+        // Clear screen
+        window.clear();
+        // Update the window
+        window.display();
+    }
+}
